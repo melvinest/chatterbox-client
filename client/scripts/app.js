b106b468420f56bc32c592835ec2bbb1a9f57df1
melvinest <melvinlouie.estrada@gmail.com>
2018-01-27 16:43:16 -0800

    added chat rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,19 +4,35 @@ class App {
   constructor() {
     this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
     this.timeStamp;
+    this.roomNames = {};
+    this.currentSetInterval;
   }
 
   init() {
-      this.fetch();
-      console.log(this);
-      $('#main').on('click', '.username', this.handleUsernameClick.bind(this));
-      $('#send').on('click', this.handleSubmit.bind(this));
-      setInterval(this.update.bind(this), 3000);
+    var application = this;
+
+    var showRoomMessages = function() {
+      event.stopPropagation();
+      console.log($(this).val());
+      var roomQuery = {where: {roomname: $(this).val()}};
+      $('#chats').empty();
+      application.fetch(roomQuery);
+      clearInterval(application.currentSetInterval);
+      application.currentSetInterval = setInterval(application.update.bind(application,roomQuery), 5000); 
+    }
+
+    this.fetch();
+    $('#main').on('click', '.username', this.handleUsernameClick.bind(this));
+    $('#send').on('click', this.handleSubmit.bind(this));
+    $('.dropdown').on('change', showRoomMessages);
+    this.currentSetInterval = setInterval(this.update.bind(this), 5000);
+
   }
+
+  
 
   send(message) {
     var application = this;
-    console.log(message)
     $.ajax({
       url: this.server,
       type: 'POST',
@@ -33,19 +49,24 @@ class App {
     });
   }
 
-  fetch() {
+  fetch(query) {
     var application = this;
+    var queryData = {order: '-createdAt'};
+
+    if (query !== undefined) {
+      _.extend(queryData, query);
+    }
+
     $.ajax({
       url: this.server,
       type: 'GET',
-      data: {order: '-createdAt'},
+      data: queryData,
       contentType: 'application/json',
 
       success: function (data) {
-        console.log('chatterbox: Message sent');
-        console.log(data);
         application.timeStamp = data['results'][0].createdAt;
         application.renderMessage(data['results']);
+      
       },
 
       error: function (data) {
@@ -54,17 +75,22 @@ class App {
     });
   }
 
-  update() {
+  update(query) {
     var application = this;
+    var queryData = {order: '-createdAt', where:{createdAt:{'$gt':{'__type':'Date','iso':application.timeStamp}}}};
+
+    if (query !== undefined) {
+      // _.extend(queryData, query);
+      queryData = {order: '-createdAt', where:{roomname: query['where'], createdAt:{'$gt':{'__type':'Date','iso':application.timeStamp}}}}
+    }
+
     $.ajax({
       url: this.server,
       type: 'GET',
-      data: {order: '-createdAt', where:{createdAt:{'$gt':{'__type':'Date','iso':application.timeStamp}}}},
+      data: queryData,
       contentType: 'application/json',
 
       success: function (data) {
-        console.log('chatterbox: Message sent');
-        console.log(data);
         if(data['results'].length > 0) {
           application.timeStamp = data['results'][0].createdAt;
           application.updateMessage(data['results']);
@@ -72,7 +98,7 @@ class App {
       },
 
       error: function (data) {
-        console.error('chatterbox: Failed to send message', data);
+        console.error('chatterbox: Failed to send message');
       }
     });
   }
@@ -80,21 +106,25 @@ class App {
   renderMessage(chats) {
     for (var i = 0; i < chats.length; i++) {
       var $userName = $('<div></div>').addClass('username').text(chats[i].username);
-      var $time = $('<div></div>').addClass('time').text(chats[i].createdAt);
-      var $message = $('<div></div>').addClass('message').text(chats[i].text);
-      // var $message = $('<div>'+chats[i].text+'</div>').addClass('message').text(chats[i].text);
+      var $time = $('<div></div>').addClass('time').text(new Date(chats[i].createdAt));
+      var $message = $('<div></div>').addClass('message').text(chats[i].text);  
       var $chatContainer = $('<div></div>').addClass('container');
+      // console.log(chats[i]['roomname'])
+      if(chats[i]['roomname'] !== undefined) {
+        this.roomNames[chats[i].roomname] = chats[i].roomname;
+      }
       $chatContainer.append($userName);
       $chatContainer.append($time);
       $chatContainer.append($message);
       $('#chats').append($chatContainer);
     }
+    this.showRoom();
   }
 
   updateMessage(chats) {
     for (var i = 0; i < chats.length; i++) {
       var $userName = $('<div></div>').addClass('username').text(chats[i].username);
-      var $time = $('<div></div>').addClass('time').text(chats[i].createdAt);
+      var $time = $('<div></div>').addClass('time').text(new Date(chats[i].createdAt));
       var $message = $('<div></div>').addClass('message').text(chats[i].text);
 
       var $chatContainer = $('<div></div>').addClass('container');
@@ -118,10 +148,25 @@ class App {
   }
 
   handleSubmit() {
+    event.stopPropagation();
     var message = { 'username': window.location.search.slice(10), 'text': $('input').val()};
-    console.log(this);
+    $('input').val('');
     this.send(message);
   }
+
+  showRoom() {
+    var val = $('.dropdown').val()
+    $('.dropdown').empty();
+    $('.dropdown').append('<option disabled selected value> -- select an option -- </option>')
+    $('.dropdown').append('<option>General</option>');
+    for (var key in (this.roomNames)){
+      console.log('this is showing');
+      var $roomName = $('<option></option>').text(this.roomNames[key]);
+      $('.dropdown').append($roomName);
+    }
+    $('.dropdown').val(val);
+  }
+
 };
 
 
